36fb326d8460c86255a26a260aad1a1827055c8f
Laurence Burden <laurenceburden@rosannas-mbp-2.lan>
2024-03-04 20:11:35 -0600

    Added score manager and 3 levels.

--- Scenes/Water/water.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-@onready var splash = $Splash
-
-func _on_body_entered(_body):
-	splash.play()
--- singletons/signal_manager.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-signal on_animal_died
-signal on_cup_destroyed
-signal on_attempt_made
